2dae921323b948f83cdc47f9f81bcdd0aa01b1c9
Adriana Pérez Conesa <aperezconesa@gmail.com>
2023-03-08 16:16:31 +0100

    pipex working not following norma

--- src/ft_pipex.c
+++ src/ft_pipex.c
@@ -6,7 +6,7 @@
 /*   By: adrperez <adrperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 13:14:24 by adrperez          #+#    #+#             */
-/*   Updated: 2023/03/08 15:41:41 by adrperez         ###   ########.fr       */
+/*   Updated: 2023/03/08 16:13:56 by adrperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,7 @@ void pipex(int infile, int outfile, char **argv, char **envp, char **paths)
 	else if (pid == 0) //hijo
 	{
 		cmd = ft_calloc(sizeof(char *), 2);
-		cmd[0] = argv[2];
-		cmd[1] = 0;
+		cmd = ft_split(argv[2], ' ');
 		printf("cmd1 %s\n", cmd[0]);
 		close(fd[0]);	// vamos ESCRIBIR en el pipe, asi que se cierra el de lectura
 		dup2(infile, STDIN_FILENO); // cierra el STDIN estandar y redirecciona el archivo al STDIN -> esto hará que infile sea el input de execve
@@ -44,8 +43,7 @@ void pipex(int infile, int outfile, char **argv, char **envp, char **paths)
 	else 
 	{
 		cmd = ft_calloc(sizeof(char *), 2);
-		cmd[0] = argv[3];
-		cmd[1] = 0;
+		cmd = ft_split(argv[3], ' ');
 		printf("cmd2 %s\n", cmd[0]);
 		pid = fork(); // segundo hijo
 		close(fd[1]); // padre lee, así que cerramos el de escritura
@@ -55,15 +53,20 @@ void pipex(int infile, int outfile, char **argv, char **envp, char **paths)
 			dup2(fd[0], STDIN_FILENO);	  // redirecciona el STDIN al fd[1] --> queremos que lea del extremo de lectura del pipe (el que el padre tiene abierto)
 			close(fd[0]);
 			execve(paths[1], cmd, envp);  // del segundo comando
+			free(cmd);
 		}
 		else if (pid == -1) // error
 			perror("Fork: ");
-		free(cmd);
-		close(fd[0]); // para que se cierre el extremo cuando ya no es necesario
+		else
+		{
+			close(fd[0]); // para que se cierre el extremo cuando ya no es necesario
+		}
 	}
 	// un wait para cada hijo
 	waitpid(pid, &status, WNOHANG);
 	waitpid(pid, &status, WNOHANG);
+	// wait(&status);
+	// wait(&status);
 	// while (1)
 	// 	;
 	// padre
